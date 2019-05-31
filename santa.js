5e4f56679d6adad556e2f9b934f948599db029cc
John Carmichael <jlcarmic@gmail.com>
2019-05-30 20:43:36 -0400

    Working solution

--- santa.js
+++ santa.js
@@ -1,3 +1,15 @@
-module.exports.deliverPresents = function (houses) {
-  console.log('Delivering presents to ' + houses[0])
+function deliverPresents(houses) {
+  if (houses.length === 1) {
+    console.log('Delivering presents to ' + houses[0])
+  } else {
+    var middle = Math.round(houses.length / 2)
+
+    var firstHalf = houses.slice(0, middle)
+    var secondHalf = houses.slice(middle, houses.length)
+
+    deliverPresents(firstHalf)
+    deliverPresents(secondHalf)
+  }
 }
+
+module.exports = { deliverPresents }
